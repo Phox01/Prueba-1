6363c397eb7184cccec3977db9daa2d794e798d5
Joseph Ruiz <josephruiz.molina@gmail.com>
2022-09-19 10:17:59 -0400

    clase del lunes semana 2 re guardado

--- Semana 2 Lunes/ejercicio8.py
+++ Semana 2 Lunes/ejercicio8.py
@@ -3,20 +3,20 @@ option= input('Por favor, escoge una opcion \n1:Vegetariana \n2:No vegetariana \
 if option=='1':
     ingr1= input('Los ingredientes disponibles son \n1:Pimenton \n2:Tofu \n')
     if ingr1== '1':
-        ingr1='Pimenton'
+        ingr1='pimenton'
     elif ingr1== '2':
-        ingr1= 'Tofu'
+        ingr1= 'tofu'
     else:
         print('Escoja un numero del 1 al 2')
     print(f'La pizza es vegetariana y tiene: Queso, Salsa y {ingr1}')
 elif option=='2':
     ingr1= input('Los ingredientes disponibles son \n1:Peperonni \n2:Jamon \n3:Salmon \n')
     if ingr1== '1':
-        ingr1='Peperonni'
+        ingr1='peperonni'
     elif ingr1== '2':
-        ingr1== 'Jamon'
+        ingr1== 'jamon'
     elif ingr1== '3':
-        ingr1== 'Salmon'
+        ingr1== 'salmon'
     else:
         print('Escoja un numero del 1 al 3')
     print(f'La pizza es no vegetariana y tiene: queso, salsa y {ingr1}')
