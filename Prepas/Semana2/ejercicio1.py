091fae4b583df4507fcf075040579e6011dcb694
Joseph Ruiz <josephruiz.molina@gmail.com>
2022-09-22 16:34:32 -0400

    preparaduría

--- Prepas/Semana2/ejercicio1.py
+++ Prepas/Semana2/ejercicio1.py
@@ -1,1 +1,9 @@
-##
+##Decir la posición de cada letra en una lista
+lista='Bienvenidos a la prepa de algoritmos'
+cont=0
+for i in lista:
+    if i=='a':
+        print(f'hay una a en el punto {cont}')
+    if i=='p':
+        print(f'hay una p en el punto {cont}')
+    cont+=1
