15547869395ed653dfe825936ccb9b3438d758e6
Joseph Ruiz <josephruiz.molina@gmail.com>
2022-10-10 08:51:18 -0400

    Semana 4

---
+++ Semana 4/Preparaduría/quiz1 ejercicio bonus.py
@@ -0,0 +1,6 @@
+#======================================================
+# Nombre del Estudiante: Joseph Ruiz
+# Carnet: 20221110023
+#======================================================
+# correo a enviar: ja.teixeira@correo.unimet.edu.ve
+
