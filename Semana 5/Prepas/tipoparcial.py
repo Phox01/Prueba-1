bbdff141c4f245faaae1007d3e3c7b67d94ee1bc
Joseph Ruiz <josephruiz.molina@gmail.com>
2022-10-13 23:21:05 -0400

    cambiosss

--- Semana 5/Prepas/tipoparcial.py
+++ Semana 5/Prepas/tipoparcial.py
@@ -1,50 +1,59 @@
+trimestre2=[]
+trimestre1=[]
+students=[]
 def main():
-    trimestre2=[]
-    trimestre1=[]
-    trim=0
-    print_welcome()
-    y=get_student_data(trim)
-    trimestre2, trimestre1, trim = trimestre(y, trimestre2, trimestre1, trim)
-    printing(y)
-
-def print_welcome():
     print("*** Bienvenido ***")
+    y=get_student_data()
+    trimestre(y, trimestre2, trimestre1)
+    printing(y)
+    quantity_students(y, students)
 
-def get_student_data(trim):
+def get_student_data():
     while True:
-        student = {
-        "cédula":input("Ingrese su cédula: "),
-        "nombre":input("Ingrese su nombre y apellido: "),
-        "promedio de notas":float(input('Ingrese su promedio: ')),
-        "trimestre": trim
-        }
+        try:
+            student = {
+            "cédula":int(input("Ingrese su cédula: ")),
+            "nombre":input("Ingrese su nombre y apellido: "),
+            "promedio de notas":float(input('Ingrese su promedio: ')),
+            "trimestre": 0
+            }
+        except SyntaxError:
+            print('Ingrese valores válidos')
+        except:
+            print('Ingrese de nuevo sus datos sin error')
+        #students.append(student)
         if input('Ingrese n para salir, otra cosa para continuar: ')=='n':
-            break 
+            break
+        
     return student
 
 
-def trimestre(student, trimestre2, trimestre1, trim):
-    x = student.get('nombre')
+def trimestre(student, trimestre2, trimestre1):
+    #for student in students:
+    x=student.get('nombre')
     if student.get('promedio de notas')>= 18:
         trimestre2.append(student)
-        #print(f'El estudiante {x} está en el trimestre 2' )
-        trim=2
+        student['trimestre']=2
     elif student.get('promedio de notas')>= 12:
         trimestre1.append(student)
-        #print(f'El estudiante {x} está en el trimestre 1' )
-        trim=1
+        student['trimestre']=1
     if student.get('promedio de notas')< 18:
         print(f'El estudiante {x} está rechazado' )
-    return trimestre1, trimestre2, trim
+    return trimestre1, trimestre2, student
+
 
 def printing(student):
+    #for student in students:
     print('La información del estudiante es:')
     print (f'Cédula: ', student.get("cédula"))
     print (f'Nombre: ', student.get("nombre"))
     print (f'Promedio de notas:', student.get("promedio de notas"))
     print (f'Su trimestre es:', student.get("trimestre"))
 
-
+def quantity_students(student, students):
+    students.append(student)
+    print('La cantidad de estudiantes que aplicaron son:', len(students))
+    return students, student
 
 ####def promedio(prom):
 ####    while True:
