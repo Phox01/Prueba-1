79085d8b25a108ee0866a8a890a790b548c84cdf
Joseph Ruiz <josephruiz.molina@gmail.com>
2022-11-30 23:58:59 -0400

    proyecto

--- Proyecto Desarrollo/Partido.py
+++ Proyecto Desarrollo/Partido.py
@@ -1,11 +1,12 @@
 class Partido:
-    def __init__(self, lteam, vteam, date, hour, estadio, id):
+    def __init__(self, lteam, vteam, date, hour, estadio, id, asistencia):
         self.lteam=lteam
         self.vteam=vteam
         self.date=date
         self.hour=hour
         self.estadio=estadio
         self.id=id
+        self.asistencia=asistencia
     def Show(self):
         print(f"{self.lteam} vs {self.vteam} en el {self.estadio} (Id:{self.id})\nFecha:{self.date} Hora:{self.hour}")
         print("---------------------------------------------------")
